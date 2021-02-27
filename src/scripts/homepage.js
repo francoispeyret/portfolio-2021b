0a6737428a2b794d16a2165d3bde9c2547945fa2
fpeyret <fpeyret@axome.com>
2021-02-27 18:29:18 +0100

    matter-js enchanded and sections integration

--- src/scripts/homepage.js
+++ src/scripts/homepage.js
@@ -13,78 +13,82 @@ let engine = Engine.create(),
     world = engine.world;
 
 let render = Render.create({
-    element: document.getElementById('home'),
+    element: document.getElementById('canvas'),
     engine: engine,
     options: {
         width: window.innerWidth,
         height: window.innerHeight,
         showAngleIndicator: false,
         background: 'transparent',
+        enableSleeping: true,
         wireframes: false
     }
 });
-
 Render.run(render);
 
 let runner = Runner.create();
 Runner.run(runner, engine);
 
-let balls = [];
-let ballOne;
-
-let ballsLength = Math.floor(window.innerWidth / 120);
-let ballOneChosen = Math.floor(ballsLength / 2);
-
-for(let i = 0; i < ballsLength; i++) {
-    let pos = {
-        x: i * 120,
-        y: render.options.height / 3
-    };
-    let radius = Math.random() * 80 + 10;
-    let options = {
-        restitution: 0.9,
-        density: 0.1 + Math.random() / 2,
-        render: {
-            fillStyle: '#222',
-            strokeStyle: null
+let ballsLength = 30;
+let ballsIndex = 0;
+let ballsStack = Composites.stack(
+    Math.floor(ballsLength/1000),
+    Math.floor(ballsLength/1000),
+    window.innerWidth/100,
+    window.innerHeight/500,
+    20,
+    20,
+    function (x,y){
+        let randomExpo = -Math.log(Math.random());
+        let radius = randomExpo * 25 + 10;
+        let ballOptions = {
+            restitution: 0.9,
+            density: 10 ,
+            render: {
+                fillStyle: '#222',
+                strokeStyle: null
+            }
+        };
+        if(ballsIndex % Math.floor(ballsLength / 4) === 0  && radius < 40) {
+            ballOptions.render.fillStyle = '#ff5127';
         }
-    };
-    if(i === ballOneChosen) {
-        options.render.fillStyle = '#eb4c25';
-        options.density = 1;
-        options.restitution = 0.75;
-        radius = 20;
+        ballsIndex++;
+        return Bodies.circle(x + radius/2, y + radius/2, radius, ballOptions);
     }
-    let ball = Bodies.circle(
-        pos.x,
-        pos.y,
-        radius,
-        options
-    );
-    if(i === ballOneChosen) {
-        ballOne = ball;
+);
+
+World.add(world, ballsStack);
+
+let ballOneOptions = {
+    radius: 20,
+    isStatic: false,
+    render: {
+        fillStyle: '#fff',
+        strokeStyle: null
     }
+};
 
-    balls.push(ball);
+let ballOne = Bodies.circle(
+    window.innerWidth/2,
+    window.innerHeight - ballOneOptions.radius * 0.75 - 50,
+    ballOneOptions.radius,
+    ballOneOptions
+);
 
-    World.add(
-        world,
-        ball
-    );
-}
+World.add(world, ballOne);
 
 const width = render.options.width,
       height = render.options.height,
-      wallsThickness = 30,
+      wallsThickness = 150,
       wallOptions = {
           isStatic: true,
           render: { visible: false }
       };
 
 World.add(world, [
-    Bodies.rectangle(width/2, -wallsThickness/2, width, wallsThickness, wallOptions),
-    Bodies.rectangle(width+wallsThickness/2, height/2, wallsThickness, height, wallOptions),
-    Bodies.rectangle(-wallsThickness/2, height/2, wallsThickness, height, wallOptions),
+    //Bodies.rectangle(width/2, -wallsThickness/2, width, wallsThickness, wallOptions),
+    Bodies.rectangle(width+wallsThickness/2, height/2, wallsThickness, height*3, wallOptions),
+    Bodies.rectangle(-wallsThickness/2, height/2, wallsThickness, height*3, wallOptions),
     Bodies.rectangle(width/2, height+wallsThickness/2, width, wallsThickness, wallOptions)
 ]);
 
@@ -98,6 +102,7 @@ let mouse = Mouse.create(render.canvas),
             }
         }
     });
+
 World.add(world, mouseConstraint);
 
 mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
@@ -105,15 +110,67 @@ mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
 
 render.mouse = mouse;
 
-document.addEventListener('wheel', function(e){
-    let scale = 1;
-    if(
-        (ballOne.circleRadius > 10 || e.deltaY > 0) &&
-        (ballOne.circleRadius < window.innerWidth/2 || e.deltaY < 0)
-    ) {
-        scale = 1 + e.deltaY/1000;
+let aboutEl = document.querySelector('section#about .block');
+let aboutBlock = Bodies.rectangle(
+    window.innerWidth*0.5,
+    aboutEl.getBoundingClientRect().top + 315,
+    1150,
+    630, {
+        friction: 0,
+        isStatic: true,
+        label: 'block',
+        render: {
+            fillStyle: 'transparent',
+            strokeStyle: null
+        }
+    });
+World.add(world, aboutBlock);
+
+let skillEls = document.querySelectorAll('section#skills .skill');
+let skillOptions = {
+    friction: 0,
+    isStatic: true,
+    label: 'blockSkill',
+    render: {
+        fillStyle: 'transparent',
+        strokeStyle: null
     }
-    if(scale < 1 || scale > 1) {
-        Matter.Body.scale(ballOne, scale, scale);
+};
+let skillsStack = [];
+for(let i = 0; i < skillEls.length; i++) {
+    const skillElRoot = document.querySelector('section#skills .skill:nth-of-type('+(i+1)+')');
+    let skillSize = {
+        w: skillElRoot.offsetWidth,
+        h: skillElRoot.offsetHeight
+    };
+    let skillPos = skillElRoot.getBoundingClientRect();
+    skillsStack.push(Bodies.rectangle(
+        skillPos.left,
+        skillPos.top,
+        skillSize.w,
+        skillSize.h,
+        skillOptions
+    ));
+}
+World.add(world, skillsStack);
+
+window.addEventListener('scroll', function(e){
+    let aboutBlockPosY = aboutEl.getBoundingClientRect().top + 315;
+    Matter.Body.set(aboutBlock, 'position', {
+        x: window.innerWidth*0.5 - 5,
+        y: aboutBlockPosY
+    });
+    for(let i = 0; i < skillsStack.length; i++) {
+        const skillElRoot = document.querySelector('section#skills .skill:nth-of-type('+(i+1)+')');
+        let skillPos = skillElRoot.getBoundingClientRect();
+        Matter.Body.set(skillsStack[i], 'position', {
+            x: skillPos.left + skillElRoot.offsetWidth/2,
+            y: skillPos.top + skillElRoot.offsetHeight/2
+        });
     }
-}, {passive: true});
+}, { passive: true });
+
+window.addEventListener('resize', function(){
+    render.canvas.width = window.innerWidth;
+    render.canvas.height = window.innerHeight;
+}, { passive: true });
